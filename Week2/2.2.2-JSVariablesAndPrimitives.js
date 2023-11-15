4d6a7437d106f3ebfe0c8ec36b8acd8467bfbae3
Cory Taylor <official.cstdesign@gmail.com>
2023-11-14 21:01:14 -0600

    JS Variables and Primitives Part 2

---
+++ Week2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,43 @@
+// create a variable called "name" that references a string
+const name = "Cory Taylor";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Waking The Demon";
+// create a variable called "wage" that references a number
+let wage = 20000;
+// create a variable called "age" that references a number
+let age = 30;
+// create a variable called "onlyChild" that references a boolean
+// eslint-disable-next-line no-unused-vars
+let onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "cars";
+console.log(typeof favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Disguise";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "video games";
+// Happy Birthday!
+// Reassign "age" to a new value
+// eslint-disable-next-line no-unused-vars
+age = 31;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+// eslint-disable-next-line no-unused-vars
+wage = 50000;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+// eslint-disable-next-line no-unused-vars
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log(
+  "Hi, my name is " +
+    name +
+    ", and my favorite song is " +
+    favoriteSong +
+    " by Bullet For My Valentine."
+);
