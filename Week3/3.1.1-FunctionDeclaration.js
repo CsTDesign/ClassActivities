552949bf42dfff99b5a4663de9808856dcf01e8e
Cory Taylor <official.cstdesign@gmail.com>
2023-11-20 19:52:57 -0600

    Function Declaration

---
+++ Week3/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+function greet() {
+  console.log("Hello!");
+}
+
+// eslint-disable-next-line no-unused-vars
+function threeModTwo() {
+  console.log(3 % 2);
+}
+
+let age = 18;
+// eslint-disable-next-line no-unused-vars
+function checkDrivingAge() {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
